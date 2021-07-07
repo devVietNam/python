bed0431c917dd5fcdd2bf64dbaf10da8df1aa4e4
devVietNam <tvtliem8984@gmail.com>
2021-07-07 12:08:15 +0700

    Add files via upload

---
+++ hello_python.py
@@ -0,0 +1,14 @@
+import re
+import uuid
+
+from pandas.io import sql
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import pandas as pd
+import argparse
+import base64
+import os
+
+msg = 'Hello World'
+print(msg)
+
